f4cd0bbb80f380dc936a7f1e23c24b27a46f1557
Lavon <Lavon_Edwards@hotmail.com>
2020-12-16 16:05:12 -0600

    Rocket Inputs Print to Console
    Doing some basic testing to make sure inputs are received properly. No movement yet, just printing to console when buttons are pressed.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Code attached to game rocket ship.
+/// Allows the rocket to thrust and rotate based on keyboard input.
+/// </summary>
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    /// <summary>
+    /// Process the input pressed.
+    /// Determine whether the ship is thrusting, or rotating left or right.
+    /// </summary>
+    private static void processInput()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            print("Thrusting");
+        }
+
+        //  Can only rotate one direction at a time.
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            print("Rotating Left");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("Rotating Right"); 
+        }
+    }
+}
